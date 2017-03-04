a1c0aa505c6b03591f22ed805069f6bc2da50766
Henrique <henrique_graca@hotmail.com>
2017-03-04 16:21:57 +0000

    removed logging service

--- myapi/Startup.cs
+++ myapi/Startup.cs
@@ -31,7 +31,7 @@ namespace myapi
         {
             //services.AddTransient<IIpsumService, IpsumService>();
             
-            services.AddLogging();
+            //services.AddLogging();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
